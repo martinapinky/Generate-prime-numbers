62c5892838b7fb46555e49595fc5ece81867be48
Martina Alinda <martinaalinda1@gmail.com>
2017-04-18 12:57:04 +0300

    changes

--- generate_prime_numbers.py
+++ generate_prime_numbers.py
@@ -1,17 +1,19 @@
 def generateprimenumbers(n):
     primenumbers = []
-    for x in range(0, n+1):
-        if x < 2:
-            r = 0
-        else:
-            i = 2
-            while i < x:
-                if x % i == 0:
-                    break
-                i = i + 1
+
+    if isinstance(n, int):
+        for x in range(0, n + 1):
+            if x < 2:
+                r = 0
             else:
-                primenumbers.append(x)
+                i = 2
+                while i < x:
+                    if x % i == 0:
+                        break
+                    i = i + 1
+                else:
+                    primenumbers.append(x)
 
     return primenumbers
 
-print(generateprimenumbers(5))
+print(generateprimenumbers(9))
